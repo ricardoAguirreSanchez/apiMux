a3e4abc5d3619b904882b2cce627184a0ded53f2
ricardoAguirreSanchez <ricardo.aguirre.99@gmail.com>
2018-12-24 15:32:23 -0300

    ahora ya puede leer archivos .txt

--- googleDrive/googleDrive.go
+++ googleDrive/googleDrive.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/oauth2"
 	"strings"
 	"encoding/json"
+	"net/http"
 	"os"
 		
 )
@@ -31,6 +32,7 @@ type DocumentoMeta struct{  //los atributos publicos
 	Title string `json:"title"`
 	MimeType string `json:"mimiType"`
 	CreatedDate string `json:"createdDate"`
+	WebContentLink string `json:"webContentLinkeatedDate"`
 }
 
 func init(){
@@ -71,16 +73,8 @@ func List() ListDocument{
 		log.Println("No tiene archivos.")
 	} else {
 			for _, i := range r.Items {
-				log.Printf("Id:%s Title:%s Mimetype:%s CreatedDate:%s\n", i.Id, i.Title,i.MimeType,i.CreatedDate)
-
-				// docu := new(DocumentoMeta)
-				// docu.Id = i.Id
-				// docu.Description = i.Description
-				// docu.MimeType = i.MimeType
-
-				docu := DocumentoMeta{i.Id,i.Title,i.MimeType,i.CreatedDate}
-
-
+				log.Printf("Id:%s Title:%s Mimetype:%s CreatedDate:%s WebContentLink:%s\n", i.Id, i.Title,i.MimeType,i.CreatedDate,i.WebContentLink)
+				docu := DocumentoMeta{i.Id,i.Title,i.MimeType,i.CreatedDate,i.WebContentLink}
 				resultado = append(resultado, docu) 
 			}
 	}
@@ -104,9 +98,9 @@ func SerchInDocument(id string,word string) string{
 	tokFile := "token.json"
 	tok, err := tokenFromFile(tokFile)
 	client := config.Client(context.Background(), tok)
-	//--------------------------------------------------------//
+	
 
-	//Busca el documento
+	//-----------------Busca el documento-------------------//
 	srv, err := drive.New(client)
 	if err != nil {
 			log.Fatalf("No se pudo recuperar el drive del cliente: %v", err)
@@ -116,23 +110,65 @@ func SerchInDocument(id string,word string) string{
 	
 	r, err := srv.Files.Get(id).Do()
 	if err != nil {
-			log.Fatalf("No se pudo recuperar el archivo error : %v", err)
+			log.Fatalf("No se pudo recuperar el archivo buscado, Error : %v", err)
 			return "No encontrado!"
 	}
 
-	//por ahora el word lo busca en el mimetype
-	if strings.Contains(r.Title, word){
-		log.Println("Existe la palabra en el titulo!")
+	//-----Descargamos y leemos el documento buscado---------------//
+	round := client.Transport
+	contenido,err := DownloadAndReadFile(round , r)
+	if err != nil {
+		log.Printf("Error al descargar el archivo: %v\n", err)
+	}else{
+		log.Printf("Descargado y lectura realizado correctamente....")
+	}
+
+	//-----Controlamos si existe el word en el contenido
+	if strings.Contains(contenido, word){
+		log.Println("Existe la palabra en el contenido!")
 		return "Encontrado!"
 	}else{
-		log.Println("No existe la palabra en el titulo :(")
+		log.Println("No existe la palabra en el contenido :(")
 		return "No encontrado!"
 	}
-	
-	//--parece que para pedir el archivo hay que mandar un request
-
-
 }
+
+
+//Permite descargar y leer archivos .txt y .pdf
+func DownloadAndReadFile(t http.RoundTripper, f *drive.File) (string, error) {
+	
+	downloadUrl := f.DownloadUrl
+	if downloadUrl == "" {
+	  // If there is no downloadUrl, there is no body
+	  log.Printf("An error occurred: File is not downloadable")
+	  return "", nil
+	}
+	req, err := http.NewRequest("GET", downloadUrl, nil)
+	if err != nil {
+		log.Printf("An error occurred: %v\n", err)
+	  return "", err
+	}
+	resp, err := t.RoundTrip(req)
+	// Make sure we close the Body later
+	defer resp.Body.Close()
+	if err != nil {
+		log.Printf("An error occurred: %v\n", err)
+	  return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("An error occurred: %v\n", err)
+	  return "", err
+	}
+
+	if strings.Contains(f.MimeType,"text/plain"){
+		return string(body), nil
+	}else{
+		//hay que cambiar el binario a texto ya que es .pdf
+		return "", nil
+	}
+}
+
 
 //https://gist.github.com/atotto/86fa30668473b41eeac7d750e5ad5f5c
 //https://stackoverflow.com/questions/46334646/google-drive-api-v3-create-and-upload-file
